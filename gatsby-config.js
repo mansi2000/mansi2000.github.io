d9e1e7b814b5989c816e88823af449fbffa5c609
Mansi2000 <mansibansal2000@gmail.com>
2021-01-05 02:00:32 +0530

    banner.png added

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     siteUrl: "https://mansi2000.github.io", // No trailing slash allowed!
     siteDescription: "Personal Blog - Mansi Bansal",
     siteLanguage: "en",
-    siteImage: "/banner.gif",
+    siteImage: "/banner.png",
     author: "Mansi Bansal",
   },
   plugins: [
